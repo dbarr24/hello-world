956cb4b3c9c0bee3d13833e6afef4ef5c56e3f9d
dbarr24 <37081957+dbarr24@users.noreply.github.com>
2018-03-05 14:18:59 -0500

    Add files via upload

--- helloWorld.py
+++ helloWorld.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
-	import sys
-	import serial
+import sys
+import serial
 	
-	ser = serial.Serial(
+ser = serial.Serial(
 		
-		port='/dev/ttyUSB0',
-		baudrate = 9600,
-		parity=serial.PARITY_NONE,
-	    stopbits=serial.STOPBITS_ONE,
-		bytesize=serial.EIGHTBITS,
-		timeout=1
+	port='/dev/ttyUSB0',
+	baudrate = 9600,
+	parity=serial.PARITY_NONE,
+	stopbits=serial.STOPBITS_ONE,
+	bytesize=serial.EIGHTBITS,
+	timeout=1
 	)
 
 while (True):
