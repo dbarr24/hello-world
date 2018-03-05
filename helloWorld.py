f69ba81b02736e4aa598de1163ce4bef88192000
dbarr24 <37081957+dbarr24@users.noreply.github.com>
2018-03-05 14:17:09 -0500

    Add files via upload

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,18 @@
-import sys
-import time
+#!/usr/bin/env python
+
+	import sys
+	import serial
+	
+	ser = serial.Serial(
+		
+		port='/dev/ttyUSB0',
+		baudrate = 9600,
+		parity=serial.PARITY_NONE,
+	    stopbits=serial.STOPBITS_ONE,
+		bytesize=serial.EIGHTBITS,
+		timeout=1
+	)
 
 while (True):
-
-	print "Yolo World"
-	time.sleep(1)
+	x = ser.readline()
+	print x
